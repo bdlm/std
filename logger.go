5c9aa4b4a607417e45691e1a3de5c8720446211f
Michael Kenney <mkenney@webbedlam.com>
2018-08-12 19:14:33 -0600

    level type

--- logger.go
+++ logger.go
@@ -1,13 +1,14 @@
 package std
 
+import (
+	"github.com/bdlm/std/logger"
+)
+
 /*
 Logger defines a common logger interface for packages that accept an
-injected logger.
-
-This interface declares that, because golang.org/pkg/log is insufficient
-(or at least cumbersome) for good visibility into high-throughput
-software in load-balanced, clustered, and/or multi-stage environments, a
-logger that is more friendly to those situations is required.
+injected logger. It fully implements the standard 'pkg/log' log package
+logging methods as well Info, Warn, Error, and Debug level equivalents.
+The Info* methods are expected to mirror the Print* methods.
 
 Packages that implement this interface should support level codes and
 expect usage as follows:
@@ -18,27 +19,38 @@ expect usage as follows:
 	- Warn, non-critical information about undesirable behavior that needs
 	  to be addressed in some way.
 	- Info, general operational information about what's happening inside an
-	  application.
+	  application. 'Print' methods should output Info-level information.
 	- Debug, usually only enabled when debugging. Add anything useful, but
 	  still exclude PII or sensitive data...
 
-Each level should include all the log levels above it. Compatible logger
-packages include:
+Each level should include all the log levels above it. To manage the output,
+this interface also implements a SetLevel(level uint) method that defines
+the log level of this logger.
 
+Compatible logger packages include:
+
+	- "github.com/bdlm/log"
 	- "github.com/sirupsen/logrus"
 */
 type Logger interface {
 
-	// Panic methods should end with a call to the panic() builtin to
-	// allow recovery.
-	Panic(args ...interface{})
-	Panicf(format string, args ...interface{})
-	Panicln(args ...interface{})
+	// SetLevel sets the log level of this logger. Accepts any unsigned
+	// integer.
+	SetLevel(level logger.Level)
 
 	// Fatal methods should call os.Exit() with a non-zero exit status.
 	Fatal(args ...interface{})
 	Fatalf(format string, args ...interface{})
 	Fatalln(args ...interface{})
+
+	// Level 1
+	// Panic methods should end with a call to the panic() builtin to
+	// allow recovery, regardless of log level, but should not attempt to
+	// write to any logs independent of that if the log level is below
+	// PanicLevel.
+	Panic(args ...interface{})
+	Panicf(format string, args ...interface{})
+	Panicln(args ...interface{})
 
 	// Level 2
 	Error(args ...interface{})
@@ -54,6 +66,10 @@ type Logger interface {
 	Info(args ...interface{})
 	Infof(format string, args ...interface{})
 	Infoln(args ...interface{})
+	// pkg/log compatiblity, alias to Info* methods.
+	Print(args ...interface{})
+	Printf(format string, args ...interface{})
+	Println(args ...interface{})
 
 	// Level 5
 	Debug(args ...interface{})
