46eb76d91ba98a2293a8c2c7a160628daa81460b
Michael Kenney <mkenney@webbedlam.com>
2026-05-21 22:33:10 -0600

    v2.1.1: Model interface cleanup

--- sorter/sorter.go
+++ sorter/sorter.go
@@ -1,17 +1,28 @@
 package sorter
 
 // SortFlag provides a type for sort flags
-type SortFlag int
+type SortFlag uint
 
 const (
-	// SortByKey - sort hash data by key
-	SortByKey SortFlag = iota
+	// SortByKey - sort hash data by key.
+	SortByKey SortFlag = 1 << iota
+
+	// SortByValue - sort data by value using type-stratified comparison:
+	// nil < bool < numeric < string < other.
+	SortByValue
+
+	// SortAsString - when sorting data use string comparison via cast. This is
+	// a tiebreak for SortByValue and the default for SortByKey.
+	SortAsString
+
+	// SortReverse - reverse the order of the data set after sorting.
+	SortReverse
 )
 
 // Sorter describes a sorter.
 type Sorter interface {
 	// Reverse reverses the order of the data set.
-	Reverse(SortFlag) error
+	Reverse() error
 	// Sort sorts the model data.
 	Sort(SortFlag) error
 	// Len returns the number of items stored in this model.
